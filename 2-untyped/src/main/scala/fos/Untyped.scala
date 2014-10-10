c362ef86c3d8288df2019e22bd48dc89df6d35d6
chernobyl88 <vincentzellweger@hotmail.com>
2014-10-10 09:31:52 +0200

    correction de l'arret
    Correction de l'arret en controlant dans la recursion qu'il y a ou non
    un changement. Problème pour le traitement des informations.

--- 2-untyped/src/main/scala/fos/Untyped.scala
+++ 2-untyped/src/main/scala/fos/Untyped.scala
@@ -89,63 +89,80 @@ object Untyped extends StandardTokenParsers {
    *  @param t the initial term
    *  @return  the reduced term
    */
-  def reduceNormalOrder(t: Term): Term = t match {
+  def reduceNormalOrder(t: Term): Term = {
+    def inner(t: Term): Term = t match {
 	    case Application(t1, t2) => checkInner(t1) match {
 	      case Abstraction(e1, t3) => t1 match {
 	        case Abstraction(e2, t4) => {
 	          subst(t4, e2, t2)
 	        }
-	        case Group(Abstraction(e2, t4)) => {
-	          subst(t4, e2, t2)
-	        }
 	        case Group(t4) => {
-	          println(Application(checkInner(t4), t2))
-	          Group(reduceNormalOrder(Application(checkInner(t4), t2)))
+	          Group(inner(Application(checkInner(t4), t2)))
 	        }
 	      }
-	      case _ => Application(reduceNormalOrder(t1), reduceNormalOrder(t2)) // <- s'arrête sur la première variable
+	      case _ => Application(inner(t1), inner(t2))
 	    }
 	    case Abstraction(e1, t1) => {
-	      Abstraction(e1, reduceNormalOrder(t1))
+	      Abstraction(e1, inner(t1))
 	    }
-	    case Group(t1) => checkInner(reduceNormalOrder(t1)) match {
-//	      case t2: Variable => {
-	//		    	  throw NoRuleApplies(t2)
-		//	    }
+	    case Group(t1) => checkInner(inner(t1)) match {
+	      case t2: Variable => {
+			    	  t2
+			    }
 	      case t2 => t2
 	    }
 	    case _ => {
-	    	 throw NoRuleApplies(t)
+	    	 t
 	    }
+    }
+    
+    var temp = inner(t);
+    
+    if (temp.toString() == t.toString()) {
+      throw NoRuleApplies(t);
+    } else {
+      temp
+    }
   }
   
   /** Call by value reducer. */
-  def reduceCallByValue(t: Term): Term = t match {
-    case Application(t1, t2) => {
-      checkInner(reduceCallByValue(t2)) match { // <- ici, la récurssion s'arrête, car arrive sur une variable et donc une exception...
-	      case Abstraction(e2, t3) =>  {
-	        checkInner(t1) match {
-			      case Abstraction(e1, t4) => {
-			        subst(Abstraction(e1, t4), e1, Abstraction(e2, t3))
-			      }
-			      case _ => {
-			        Application(reduceCallByValue(t1), reduceCallByValue(t2))
+  def reduceCallByValue(t: Term): Term = {
+    def inner(t: Term): Term = t match {
+	    case Application(t1, t2) => {
+	      checkInner(inner(t2)) match {
+		      case Abstraction(e2, t3) =>  {
+		        checkInner(t1) match {
+				      case Abstraction(e1, t4) => {
+				        subst(Abstraction(e1, t4), e1, Abstraction(e2, t3))
+				      }
+				      case _ => {
+				        Application(inner(t1), inner(t2))
+				      }
 			      }
 		      }
+		      case _ => {
+		        Application(inner(t1), inner(t2))
+		      }
 	      }
-	      case _ => {
-	        Application(reduceCallByValue(t1), reduceCallByValue(t2))
-	      }
-      }
+	    }
+	    case Abstraction(e1, t1) => Abstraction(e1, inner(t1))
+	    case Group(t1) => checkInner(inner(t1)) match {
+	      case e: Variable => e
+	      case t2 => Group(t2)
+	    }
+	    case _ => {
+	      t
+	    }
+	  }
+    
+    var temp = inner(t);
+    
+    if (temp.toString() == t.toString()) {
+      throw NoRuleApplies(t);
+    } else {
+      temp
     }
-    case Abstraction(e1, t1) => Abstraction(e1, reduceCallByValue(t1)) // Est conscidéré comme une valeur. Doit-il être traité quand même?
-    case Group(t1) => checkInner(reduceCallByValue(t1)) match {
-//      case e: Variable => throw NoRuleApplies(e) 
-      case t2 => Group(t2)
-    }
-    case _ => {
-      throw NoRuleApplies(t) // Cause de l'exception
-    }
+    
   }
 
   /** Returns a stream of terms, each being one step of reduction.
@@ -166,38 +183,19 @@ object AllDone extends Exception { }
 
   def main(args: Array[String]): Unit = {
     
-    //var myData = "((\\x. x) y) (\\x. ((((\\y. y)))) ((\\z. z) ((\\ w. w) \\t. t)))";
-    var myData = "((\\x. x) y)";
+    var myData = "((\\x. x)) (\\x. ((((\\y. y)))) ((\\z. z) ((\\ w. w) \\t. t)))";
+    //var myData = "((\\x. x) y)";
     val tokens = new lexical.Scanner(myData)
-    var previous = ""
     System.out.println("----------------------------------------------------------");
    // val tokens = new lexical.Scanner(StreamReader(new java.io.InputStreamReader(System.in)))
     phrase(Term)(tokens) match {
       case Success(trees, _) =>
         println("normal order: ")
-        try {
-          for (t <- path(trees, reduceNormalOrder)){
-          if(previous == t.toString()){
-            throw AllDone
-          } else {
-        previous = t.toString()
+        for (t <- path(trees, reduceNormalOrder))
           println(t)
-          }}
-          } catch {
-          	case AllDone =>
-          }
         println("call-by-value: ")
-        try {
-          for (t <- path(trees, reduceCallByValue)){
-          if(previous == t.toString()){
-            throw AllDone
-          } else {
-        previous = t.toString()
+        for (t <- path(trees, reduceCallByValue))
           println(t)
-          }}
-          } catch {
-          	case AllDone =>
-          }
 
       case e =>
         println(e)
