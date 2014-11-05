9eda61908b7ac8299df34f6d6b415c93bad8b972
chernobyl88 <vincentzellweger@hotmail.com>
2014-11-05 10:39:40 +0100

    nothing

--- 3-typed/src/main/scala/fos/SimplyTyped.scala
+++ 3-typed/src/main/scala/fos/SimplyTyped.scala
@@ -157,6 +157,7 @@ object SimplyTyped extends StandardTokenParsers {
 
   def main(args: Array[String]): Unit = {
     var myData = "(fix (\\y: Nat->Bool. \\x:Nat. if iszero x then true else if iszero (pred x) then false else (y (pred(pred x))))) 7";
+    var myData = ""
      //var myData = "((\\y. y) \\y.y)";
      val tokens = new lexical.Scanner(myData)
    // val tokens = new lexical.Scanner(StreamReader(new java.io.InputStreamReader(System.in)))
