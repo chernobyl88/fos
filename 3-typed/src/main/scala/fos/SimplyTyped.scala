656f672b2717a98af4d03ef14f961c66ca392d50
chernobyl88 <vincentzellweger@hotmail.com>
2014-11-05 11:01:14 +0100

    maj parser

--- 3-typed/src/main/scala/fos/SimplyTyped.scala
+++ 3-typed/src/main/scala/fos/SimplyTyped.scala
@@ -8,10 +8,10 @@ import scala.util.parsing.input._
  *  the TAPL book.
  */
 object SimplyTyped extends StandardTokenParsers {
-  lexical.delimiters ++= List("(", ")", "\\", ".", ":", "=", "->", "{", "}", ",", "*", "+", "|")
+  lexical.delimiters ++= List("(", ")", "\\", ".", "=>", ":", "=", "->", "{", "}", ",", "*", "+", "|")
   lexical.reserved   ++= List("Bool", "Nat", "true", "false", "if", "then", "else", "succ",
                               "pred", "iszero", "let", "in", "fst", "snd", "inl", "inr",
-                              "case", "fix", "letrec", "=>", "of", "as")
+                              "case", "fix", "letrec", "of", "as")
 
   /** Term     ::= SimpleTerm { SimpleTerm }
    */
@@ -41,7 +41,7 @@ object SimplyTyped extends StandardTokenParsers {
 	| "{" ~> Term ~","~ Term <~ "}" ^^ { case e1 ~","~ e2 => Pair(e1,e2)}
 	| "fst" ~> Term ^^ { case e1 => First(e1)}
 	| "snd" ~> Term ^^ { case e1 => Second(e1)}
-	| "case" ~ Term ~ "of inl" ~ ident ~ "=>" ~ Term  ~ "| inr" ~ ident ~ "=>" ~ Term ^^{ case "case" ~ t ~ "of inl" ~ x1 ~ "=>" ~ t1  ~ "| inr" ~ x2 ~ "=>" ~ t2 =>Case(t,Variable(x1),t1,Variable(x2),t2)}
+	| "case" ~ Term ~ "of" ~ "inl" ~ ident ~ "=>" ~ Term  ~ "|" ~ "inr" ~ ident ~ "=>" ~ Term ^^{ case "case" ~ t ~ "of" ~ "inl" ~ x1 ~ "=>" ~ t1  ~ "|" ~ "inr" ~ x2 ~ "=>" ~ t2 =>Case(t,Variable(x1),t1,Variable(x2),t2)}
     | "inl" ~ Term  ~ "as" ~ Type ^^{ case "inl" ~ e1  ~ "as" ~ t1 => Inl(e1,t1)}
 	| "inr" ~ Term  ~ "as" ~ Type ^^{ case "inr" ~ e1  ~ "as" ~ t1 => Inr(e1,t1)}
 	| "fix" ~ Term  ^^ { case "fix" ~ e1 => Fix(e1)}
@@ -159,7 +159,7 @@ object SimplyTyped extends StandardTokenParsers {
   def main(args: Array[String]): Unit = {
     var myData = "(fix (\\y: Nat->Bool. \\x:Nat. if iszero x then true else if iszero (pred x) then false else (y (pred(pred x))))) 7";
     myData = ""
-    myData = "case inl true as Nat of inl x => x  | inr y => y";
+    myData = "case inl true as Bool of inl x => true  | inr y => false";
      //var myData = "((\\y. y) \\y.y)";
      val tokens = new lexical.Scanner(myData)
    // val tokens = new lexical.Scanner(StreamReader(new java.io.InputStreamReader(System.in)))
